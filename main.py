26eaaaf3b8fff85323b4127f9803ed7a3c0213a5
Nabin <sharmanabin.0910@gmail.com>
2025-11-15 20:20:47 +0530

    extract_schema

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 # Extract Schema 
 
 from sqlalchemy import create_engine, inspect
+import json
 
-db_url = 'sqlite://amazon.db'
+db_url = 'sqlite:///amazon.db'
 
-engine = create_engine(db_url)
-inspector = inspect
- 
+def extract_schema(db_url) : 
+    engine = create_engine(db_url)
+    inspector = inspect(engine)
+    schema = {}
+
+    for table_name in inspector.get_table_names():
+        columns = inspector.get_columns(table_name)
+        schema[table_name] = [col['name'] for col in columns]
+
+    return json.dumps(schema)
+    
+    
 # Text to SQL (LLM with Ollama)
 
 # Build StreamLit frontend 
